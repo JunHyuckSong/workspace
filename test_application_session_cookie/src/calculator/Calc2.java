b88c16880ffd9488998dd3947a0b5ea50b317c7f
JunHyuckSong <westjun49@naver.com>
2021-11-22 18:47:20 +0900

    서블릿으로 세션/쿠키/어플리케이션 연습

--- test_application_session_cookie/src/calculator/Calc2.java
+++ test_application_session_cookie/src/calculator/Calc2.java
@@ -1,17 +1,16 @@
 package calculator;
 
 import java.io.IOException;
-import java.util.Iterator;
 
 import javax.script.ScriptEngine;
-import javax.servlet.ServletContext;
+import javax.script.ScriptEngineManager;
+import javax.script.ScriptException;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.servlet.http.HttpSession;
 
 /**
  * Servlet implementation class Calc
@@ -37,9 +36,19 @@ public class Calc2 extends HttpServlet {
 		
 		if(operator != null && operator.equals("=")) {
 			// 이 부분을 구현해야 함
+			ScriptEngine engine = new ScriptEngineManager().getEngineByName("nashorn");
+			try {
+				exp = String.valueOf(engine.eval(exp));
+			} catch (ScriptException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
 			
+		}else if(operator != null && operator.equals("C")) {
+			// 쿠키를 지우는 부분
+			exp = "";// 쿠키를 초기화하는 것은 맞지만 완벽한 초기화는 아님
 			
-			
+						
 		}else {			
 			// 밑 3개 중 cookie가 넘어올 떈 하나씩박에 넘어오지 않으므로 3개중 한개만 실행됨.
 			exp += (value == null)?"":value; // 1
@@ -49,7 +58,9 @@ public class Calc2 extends HttpServlet {
 		
 		
 		Cookie expCookie = new Cookie("exp", exp);
-		
+		if(operator != null && operator.equals("C")) { // 이 부분을 통해 완벽히 쿠키를 지울 수 있음.
+			expCookie.setMaxAge(0);
+		}
 		response.addCookie(expCookie);
 		response.sendRedirect("CalcPage");
 		
